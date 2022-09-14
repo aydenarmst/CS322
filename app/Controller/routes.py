1375dac7ce5f8367b9ba7bcca617af174064752d
aarmstro <ayden.armstrong@wsu.edu>
2022-09-14 14:52:01 -0700

    Verison v.03 - editing the forms.py to add the smile message body in the PostForm, editing create.html to add text input fields for the post form. Adding a new route in routes.py for the postform.

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -17,3 +17,16 @@ bp_routes.template_folder = Config.TEMPLATE_FOLDER #'..\\View\\templates'
 def index():
     posts = Post.query.order_by(Post.timestamp.desc())
     return render_template('index.html', title="Smile Portal", posts=posts.all())
+
+
+@bp_routes.route('/postsmile/', methods=['GET', 'POST'])
+def postsmile():
+    pform = PostForm()
+    if pform.validate_on_submit():
+        newPost = Post(title = pform.title.data, body = pform.body.data, happiness_level = pform.happiness_level.data)
+        db.session.add(newPost)
+        db.session.commit()
+        flash("Title " + newPost.title + " is created")
+        return redirect(url_for('routes.index'))
+
+    return render_template('create.html', form=pform)
