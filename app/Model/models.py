c608b0aa9085ba314524cf744e232d81e027ca2e
aarmstro <ayden.armstrong@wsu.edu>
2022-10-03 14:22:14 -0700

    Version v.07 - Revising the DB models.py, adding the user table with werkzurg security generate password hash's.

--- app/Model/models.py
+++ app/Model/models.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from enum import unique
 from app import db
 
 
@@ -47,6 +49,20 @@ class Tag(db.Model):
     def __repr__(self):
         return '<ID: {} - Name: {}>'.format(self.id, self.name)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique=True, index = True)
+    email = db.Column(db.String(120), unique=True, index = True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {} - {}:>'.format(self.id,self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 
